cbf67f59c3245d991beac516673aa76048e2dd7b
ZakVados <c0H7W2c3#mPZD$2Iuc4B0LbnruPGwi>
2023-01-03 16:07:02 +0200

    MethodParser done

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,11 @@
 package com.epam.mjc;
 
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringJoiner;
+import java.util.StringTokenizer;
+
 public class MethodParser {
 
     /**
@@ -19,7 +25,54 @@ public class MethodParser {
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
+
+    public static final List<String> ACCESS_MODIFIERS = List.of("public", "protected", "private");
+
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        String accessModifier = null;
+        String returnType;
+        String methodName;
+        List<MethodSignature.Argument> arguments = new ArrayList<>();
+
+        // signature and parameters preparing (tokenizing):
+        List<String> signNParam = MethodParserTokenizer(signatureString, "()");
+        List<String> sign = MethodParserTokenizer(signNParam.get(0), " ");
+        if (signNParam.size() > 1) {
+            List<String> param = MethodParserTokenizer(signNParam.get(1), " ");
+            List<String> params = MethodParserTokenizer(signNParam.get(1), ",, ");
+            for (int i = 0; i < params.size(); i+=2) {
+                System.out.print("type -> " + params.get(i));
+                System.out.println(" name -> " + params.get(i+1));
+                MethodSignature.Argument arg = new MethodSignature.Argument(params.get(i), params.get(i+1));
+                arguments.add(arg);
+            }
+        }
+
+        // check optional access modifiers:
+        if (ACCESS_MODIFIERS.contains(sign.get(0))) {
+            accessModifier = sign.get(0);
+            sign.remove(0);
+        }
+        // after deleting access modifiers unification ret type and name setting:
+        returnType = sign.get(0);
+        methodName = sign.get(1);
+
+        MethodSignature ms = new MethodSignature(methodName, arguments);
+        ms.setReturnType(returnType);
+        ms.setAccessModifier(accessModifier);
+
+        return ms;
     }
+
+    public List<String> MethodParserTokenizer(String stringToParse, String delimiter) {
+        // signature and parameters preparing (tokenizing)
+        List<String> elements = new ArrayList<>();
+        StringTokenizer signMParamTokenizer = new StringTokenizer(stringToParse, delimiter);
+        while (signMParamTokenizer.hasMoreTokens()) {
+            elements.add(signMParamTokenizer.nextToken());
+        }
+        return elements;
+    }
+
 }
